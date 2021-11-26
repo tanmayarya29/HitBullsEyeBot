ec4e90a55660e16499dce81952d800231dbddc21
Tanmay Mishra <tanmayarya29@gmail.com>
2021-11-26 17:24:38 +0530

    level3

--- hbe.py
+++ hbe.py
@@ -39,9 +39,10 @@ def get_details():
     global pswd
     root.withdraw()
     if os.path.isfile('login_up.txt'):
-        f=open('login_up.txt','r')
+        f=open('login_up.txt','r+')
         usrnm=f.readline()
         pswd=f.readline()
+        f.close()
     else:
         f=open('login_up.txt','w')
         usrnm=simpledialog.askstring(title="Username",prompt="Enter the Username here:")
@@ -49,7 +50,7 @@ def get_details():
         pswd=simpledialog.askstring(title="Password",prompt="Enter the Password here:")
         f.write("p-"+pswd)
         f.close()
-        f=open('login_up.txt','r')
+        f=open('login_up.txt','r+')
         usrnm=f.readline()
         pswd=f.readline()
         f.close()
@@ -75,6 +76,7 @@ def type():
 
 #list tests 
 def list_tests():
+    time.sleep(2)
     global rem_test_names,rem_test_start
     rem_test_names=[]
     rem_test_start=[]
@@ -85,13 +87,11 @@ def list_tests():
             rem_test_start.append(i.find_elements_by_tag_name('a'))
     for i in range(len(rem_test_names)):
         print(str(i+1)+" - "+rem_test_names[i])
-    # print(rem_test_names)
     print(rem_test_start)
     rem_test_start[0][0].click()
     driver.find_element_by_xpath('//*[@id="instPaginationa"]').click()
     driver.find_element_by_xpath('//*[@id="disclaimer"]').click()
     driver.find_element_by_xpath('//*[@id="secondPagep2"]/div/input').click()
-#start -> next (//*[@id="instPaginationa"]) -> t&c(//*[@id="disclaimer"]) -> startTest(//*[@id="secondPagep2"]/div/input)
 def attempt():
     anskey=[]
     ansfile=open('anskey.txt','r')
@@ -101,44 +101,22 @@ def attempt():
     print(anskey)
     time.sleep(1)
     for i in range(len(anskey)):
-        if(anskey[i]=="A"):
+        if(anskey[i]=="A" or anskey[i]=="a"):
             driver.find_element_by_xpath('//*[@id="A_'+str(i+1)+'"]').click()
-        elif (anskey[i]=="B"):
+        elif (anskey[i]=="B" or anskey[i]=="b"):
             driver.find_element_by_xpath('//*[@id="B_'+str(i+1)+'"]').click()
-        elif (anskey[i]=="C"):
+        elif (anskey[i]=="C" or anskey[i]=="c"):
             driver.find_element_by_xpath('//*[@id="C_'+str(i+1)+'"]').click()
-        else:
+        elif (anskey[i]=="D" or anskey[i]=="d"):
             driver.find_element_by_xpath('//*[@id="D_'+str(i+1)+'"]').click()
         driver.find_element_by_xpath('//*[@id="foot"]/div[1]/div[3]/a').click()
     driver.find_element_by_xpath('//*[@id="demo-page"]/div[1]/a[2]').click()
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="activator"]/a').click()
     time.sleep(1)
-    # driver.find_element_by_xpath('//*[@id="radio-choice-0a"]').click()
     driver.find_element_by_xpath('//*[@id="popupLogin"]/div/form/div[1]/label/span').click()
     time.sleep(1)
     driver.find_element_by_xpath('//*[@id="close_confirmed"]').click()
-
-
-#options q1
-#//*[@id="A_1"] //*[@id="D_1"]
-#//*[@id="B_1"]
-#//*[@id="C_1"]
-#//*[@id="D_1"]
-##-> next->(//*[@id="foot"]/div[1]/div[3]/a)
-#and cycle continues
-
-
-#after done 3lines right
-#//*[@id="demo-page"]/div[1]/a[2]
-#submit test
-#//*[@id="activator"]/a
-#finished test chkbox
-#//*[@id="radio-choice-0a"]
-#submitfinal
-#//*[@id="close_confirmed"]
-
-
 def main():
     login()
     time.sleep(2)
@@ -147,7 +125,7 @@ def main():
     driver.find_element_by_xpath('/html/body/app/body/div/div/div[2]/app-testzone/app-testzonemenus/div/div/div/div[4]/div[1]/a').click()
     time.sleep(2)
     type()
-    time.sleep(3)
+    time.sleep(2)
     list_tests()
     time.sleep(3)
     attempt()
